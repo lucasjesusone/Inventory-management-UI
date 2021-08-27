73971fa175817b1ff526daf20f9938e2c8be600b
lucasjesusone <lucasjesusone@gmail.com>
2021-08-26 23:52:39 -0400

    Creating form with reactive forms

--- src/app/pages/invoices/invoice-list/invoice-list.component.ts
+++ src/app/pages/invoices/invoice-list/invoice-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { timingSafeEqual } from 'crypto';
 
 @Component({
   selector: 'app-invoice-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invoice-list.component.scss']
 })
 export class InvoiceListComponent implements OnInit {
+  form: FormGroup
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+  }
+
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      
+    })
   }
 
 }
