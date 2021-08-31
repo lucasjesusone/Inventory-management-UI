98324d682874f21fcdebd3743c8a72f7d1149129
lucasjesusone <lucasjesusone@gmail.com>
2021-08-30 23:50:28 -0400

    Creating product and client modules

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,12 +2,10 @@ import { PagesComponent } from "./pages.component";
 import { PagesRoutingModule } from "./pages-routing.module";
 import { Injector, NgModule } from "@angular/core";
 import { NbMenuItem, NbMenuModule } from "@nebular/theme";
-import { ProductsComponent } from './products/products.component';
-import { ClientsComponent } from './clients/clients.component';
 
 @NgModule({
   imports: [PagesRoutingModule, NbMenuModule],
-  declarations: [PagesComponent, ProductsComponent, ClientsComponent],
+  declarations: [PagesComponent],
 })
 export class PagesModule {
   static injector: Injector;
