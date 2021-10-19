c39e517137e0f62bcc443e6f850d17d3687062ec
lucasjesusone <lucasjesusone@gmail.com>
2021-10-18 22:35:11 -0400

    adding client component

--- src/app/pages/clients/client.module.ts
+++ src/app/pages/clients/client.module.ts
@@ -1,12 +1,44 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatOptionModule } from "@angular/material/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSelectModule } from "@angular/material/select";
+import { MatTableModule } from "@angular/material/table";
+import {
+  NbButtonModule,
+  NbTooltipModule,
+  NbIconModule,
+  NbCardModule,
+} from "@nebular/theme";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { InvoiceRoutingModule } from "../invoices/invoice-routing.module";
+import { ClientRoutingModule } from "./client-routing.module";
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    NgbModule,
+    MatTableModule,
+    MatPaginatorModule,
+    ClientRoutingModule,
+    NbButtonModule,
+    NbTooltipModule,
+    NbIconModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatOptionModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatInputModule,
+    NbCardModule,
+  ],
+  declarations: [ClientRoutingModule.components],
 })
-export class ClientModule { }
+export class ClientModule {}
