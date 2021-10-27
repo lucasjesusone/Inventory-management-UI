65fc57b6789218e84e12a2efdfa12b1fd22e617c
lucasjesusone <lucasjesusone@gmail.com>
2021-10-26 23:54:16 -0400

    updating client services

--- src/app/pages/clients/client-list/client-list.component.ts
+++ src/app/pages/clients/client-list/client-list.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { ClientService } from './../client.service';
+import { ActivatedRoute, Router } from "@angular/router";
+import { ClientModel } from "./../client.model";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-client-list',
-  templateUrl: './client-list.component.html',
-  styleUrls: ['./client-list.component.scss']
+  selector: "app-client-list",
+  templateUrl: "./client-list.component.html",
+  styleUrls: ["./client-list.component.scss"],
 })
 export class ClientListComponent implements OnInit {
+  title: string;
+  displayedColumns: string[] = [
+    "razaoSocial",
+    "inscricaoEstadual",
+    "cnpj",
+    "cidade",
+    "uf",
+    "status",
+    "acoes",
+  ];
 
-  constructor() { }
+  dataSource: ClientModel[];
 
-  ngOnInit(): void {
+  invoices: ClientModel[] = [];
+  constructor(private activatedRoute: ActivatedRoute, private service: ClientService, private router: Router) {
+    this.activatedRoute.data.subscribe((data) => {
+      this.title = data.title;
+    });
   }
 
+  ngOnInit(): void {
+    this.getAllInvoices()
+  }
+
+  getAllInvoices() {
+    this.service.getAll().subscribe((res) => {
+      this.invoices = res;
+      this.dataSource = res;
+    });
+  }
+
+  goToNew() {
+    this.router.navigate(["/pages/client/new"]);
+  }
 }
--- src/app/pages/clients/client.model.ts
+++ src/app/pages/clients/client.model.ts
@@ -1,2 +1,8 @@
 export class ClientModel {
+    razaoSocial: string;
+    inscricaoEstadual: number
+    cnpj: number
+    cidade: string
+    uf: string
+    status: string
 }
